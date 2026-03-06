152526bd019035689f910a187e9f16bd51bf2535
Matheus <matheusmorante002@gmail.com>
2026-03-06 00:13:19 -0300

    '222'

--- pdv/src/Router.tsx
+++ pdv/src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import ReceiptPage from './pages/ReceiptPage';
+import ReceiptPage from './pages/ReceiptPage/index';
 import WarrantyTermPage from './pages/WarrantyTermPage';
 import DeliverySchedule from './pages/App/DeliverySchedule';; 
 import SalesOrder from './pages/App/SalesOrder';
