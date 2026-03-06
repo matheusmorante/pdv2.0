f7c8871bb013ecb1d665dbd602c07a76488de0fd
Matheus <matheusmorante002@gmail.com>
2026-03-05 23:40:49 -0300

    '3'

--- pdv/src/pages/App/SalesOrder/CustomerData.tsx
+++ pdv/src/pages/App/SalesOrder/CustomerData.tsx
@@ -122,7 +122,7 @@ const CustomerDataInputs = ({ customerData, setCustomerData }: Props) => {
                     />
                 </div>
                 <div className="flex flex-col">
-                    <label>Observações sobre o endereço</label>
+                    <label>Observaçoes sobre o endereço</label>
                     <input
                         name="observation"
                         onChange={e => onChangeAddress('observation', e.target.value)}
